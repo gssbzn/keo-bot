1c55e3a33fb559acde8c5851357a0826fe025d18
Gustavo Bazan <gustavo.bazan@koombea.com>
2015-06-11 14:15:40 -0500

    add express listening for index

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var Slack = require('slack-client');
 var Keo = require('./keo.js');
+var express = require('express');
+var app = express();
 
 var uristring =
   process.env.MONGOLAB_URI ||
@@ -31,3 +33,14 @@ slack.on('error', function(error) {
 });
 
 slack.login();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
